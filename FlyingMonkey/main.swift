86c08f0e54b3d359ae1ede17db5820159761ec3a
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-08-18 11:40:06 -0400

    Initial Commit

---
+++ FlyingMonkey/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FlyingMonkey
+//
+//  Created by Ahmad Alhashemi on 2017-08-18.
+//  Copyright © 2017 Ahmad Alhashemi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
