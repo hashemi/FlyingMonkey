bbe89c788e8bcc8a6b1a2bf958bf99574617aac8
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-08-18 11:49:32 -0400

    Add a test target

---
+++ FlyingMonkeyTests/FlyingMonkeyTests.swift
@@ -0,0 +1,13 @@
+//
+//  FlyingMonkeyTests.swift
+//  FlyingMonkeyTests
+//
+//  Created by Ahmad Alhashemi on 2017-08-18.
+//  Copyright © 2017 Ahmad Alhashemi. All rights reserved.
+//
+
+import XCTest
+
+class FlyingMonkeyTests: XCTestCase {
+
+}
